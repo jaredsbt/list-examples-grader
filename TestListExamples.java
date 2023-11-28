f89168d8d42bfaf9c1bee86b909fb4b4a6b5f475
Jared Rosas <jaredsbt@gmail.com>
2023-11-28 10:55:58 -0800

    Updated grade bash

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,6 +9,12 @@ class IsMoon implements StringChecker {
   }
 }
 
+class hasHe implements StringChecker {
+  public boolean checkString(String s) {
+    return s.contains("He");
+  }
+}
+
 public class TestListExamples {
   @Test(timeout = 500)
   public void testMergeRightEnd() {
@@ -17,5 +23,21 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testFilter1() {
+    List<String> expected = Arrays.asList("Hey", "Hello");
+    List<String> input1 = Arrays.asList("Hey", "Hello", "Hi");
+    hasHe sc = new hasHe();
+    assertEquals(expected, ListExamples.filter(input1, sc));
+  }
+
+  @Test
+  public void testMoon1(){
+    List<String> expected = Arrays.asList("MOON", "Moon");
+    List<String> input1 = Arrays.asList("MOON", "MUNE", "MINE", "Moon");
+    IsMoon sc = new IsMoon();
+    assertEquals(expected, ListExamples.filter(input1, sc));
   }
 }
--- grade.sh
+++ grade.sh
@@ -1,6 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission
+# rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
@@ -8,9 +8,26 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f 'student-submission/ListExamples.java' ]]
+    then
+        echo "Correct file. Moved to grading-area"
+        cp "student-submission/ListExamples.java" "grading-area"
+        cp "TestListExamples.java" "grading-area"
+else
+    echo "Incorrect file type. Must be java"
+    exit 1
+fi
+
+cd grading-area
+
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# java TestListExamples1 > output.txt
