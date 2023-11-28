0dc57f0e7f7d5acbd6e3d44d5efaf922d2f00a8b
Jared Rosas <jaredsbt@gmail.com>
2023-11-28 12:57:15 -0800

    Grader bash script successful

--- grade.sh
+++ grade.sh
@@ -1,6 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-# rm -rf student-submission
+rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
@@ -30,4 +30,30 @@ java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
 
-# java TestListExamples1 > output.txt
+if ! grep -q "Failures" output.txt; 
+    then
+        echo "100% test complete"
+    
+else
+    echo "You have some failures"
+
+    line=`grep "Tests" output.txt`
+    testsCheck=""
+    numOfTests=0
+    numOfFails=0
+
+    # iterates words instead of characters, allows for loop to check characters past the colons (Ex. Failures: 3)
+    IFS=', ' read -ra words <<< "$line"
+
+    echo $line
+    for char in "${words[@]}"; do 
+        if [[ $char =~ ^[0-9]+$ ]] && [[ $testsCheck == "" ]]; then
+            numOfTests=$char
+            testsCheck+="Check"
+        elif [[ $char =~ ^[0-9]+$ ]]; then
+            numOfFails=$char
+        fi 
+    done
+    
+    echo "Tests passed: $((numOfTests - numOfFails)) / $numOfTests"
+fi
